d03dad31e40aa22845cd643d83f38b1bae22886e
hiteshparmar16320@gmail.com <hiteshparmar16320@gmail.com>
2022-12-14 22:45:01 +0530

    sleek-circular-bar-finished

--- lib/widgets/sleek_circular_slider.dart
+++ lib/widgets/sleek_circular_slider.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: must_be_immutable, implementation_imports, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:weather_app/Colors/custom-colors.dart';
 import 'package:weather_app/Global_Controller/Global_Controller.dart';
 import 'package:weather_app/models/weather/current.dart';
 
@@ -22,25 +23,96 @@ class SleekCircularWidget extends StatelessWidget {
       children: [
         Container(
           margin: EdgeInsets.only(
-            top: 10,
+            top: 20,
             left: 20,
             right: 20,
             bottom: 20,
           ),
           child: SizedBox(
-            height: 180,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Center(
-                child: SleekCircularSlider(
-                  min: 0,
-                  max: 100,
-                  initialValue: weatherDataCurrent.current.humidity!.toDouble(),
+            // height: 180,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: SleekCircularSlider(
+                    min: 0,
+                    max: 100,
+                    initialValue:
+                        weatherDataCurrent.current.humidity!.toDouble(),
+                    appearance: CircularSliderAppearance(
+                      infoProperties: InfoProperties(
+                          bottomLabelText: "Humidity",
+                          topLabelStyle:
+                              GoogleFonts.abhayaLibre(color: Colors.amber)),
+                      animationEnabled: true,
+                      size: 180,
+                      customColors: CustomSliderColors(
+                          hideShadow: true,
+                          dotColor: Colors.yellow.shade900,
+                          progressBarColors: [
+                            CustomColors.firstGradientColor,
+                            CustomColors.secondGradientColor,
+                          ],
+                          trackColors: [
+                            CustomColors.firstGradientColor,
+                            CustomColors.secondGradientColor,
+                          ]),
+                    ),
+                  ),
                 ),
-              ),
-            ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Feels Like ",
+                            style: GoogleFonts.barlow(
+                              color: CustomColors.textColorBlack.withAlpha(
+                                500,
+                              ),
+                            ),
+                          ),
+                          TextSpan(
+                            text: "${weatherDataCurrent.current.tempC}°c",
+                            style: GoogleFonts.barlow(
+                              color: CustomColors.textColorBlack.withAlpha(
+                                500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "UV index ",
+                            style: GoogleFonts.barlow(
+                              color: CustomColors.textColorBlack.withAlpha(
+                                500,
+                              ),
+                            ),
+                          ),
+                          TextSpan(
+                            text: "${weatherDataCurrent.current.uv}",
+                            style: GoogleFonts.barlow(
+                              color: CustomColors.textColorBlack.withAlpha(
+                                500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
